a9996729bbbdcd81ec98bfb273f8f6e06b619221
AbdushukurRasulov <abdushukur9717@mail.ru>
2020-05-29 02:02:07 +0500

    Node bootcamp course: Overview and Tour views finished

---
+++ routes/viewRoutes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const { getOverview, getTour } = require('../controllers/viewController');
+
+router.get('/', getOverview)
+router.get('/tour/:slug', getTour)
+
+module.exports = router;
